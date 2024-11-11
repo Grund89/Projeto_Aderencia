2864cfc5aee54317387464b489ebc6bb2ddbff26
Douglas Grund <douglasgrund@yahoo.com.br>
2024-11-11 15:45:54 -0300

    feat: definido as rotas em config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
+```ruby
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'dashboard#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :machines do
+    member do
+      post :register_delay
+      post :reset_delay
+    end
+  end
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :production_orders
 end
+```
